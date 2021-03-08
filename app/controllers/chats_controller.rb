24bfe19a32f83de6f6603cc2ab3a7e70dc348da9
akahito1006 <nekodora5k@gmail.com>
2021-03-08 02:18:14 +0000

    [Update] 応用課題8

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -20,7 +20,7 @@ class ChatsController < ApplicationController
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
-    redirect_to request.referer
+
   end
   
   private
